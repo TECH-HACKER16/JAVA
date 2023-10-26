06b602d736ded3404e72ea2a2329a52ee8bb2ac3
Venkata Teja <142000714+TECH-HACKER16@users.noreply.github.com>
2023-10-26 19:20:12 +0530

    Using bitwise operator for finding even and odd intergers

---
+++ Questions/Even_or_odd.java
@@ -0,0 +1,23 @@
+package Questions;//comment this line if error occurs
+
+import java.util.Scanner;
+
+public class Even_or_odd 
+{
+    void fn()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num=sc.nextInt();
+        if ((num & 1) == 0)
+            System.out.println("\n"+num+" is an even integer\n");
+        else
+            System.out.println("\n"+num+" is an odd integer\n");
+        sc.close();
+    }
+    public static void main(String[] args) 
+    {
+        Even_or_odd obj = new Even_or_odd();
+        obj.fn();
+    }  
+}
